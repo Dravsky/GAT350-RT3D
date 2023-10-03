78cc7fde8d81f5c6780be1fc327fd45246d776ff
Dravsky <129130564+Dravsky@users.noreply.github.com>
2023-10-03 11:40:23 -0600

    Add project files.

---
+++ Source/Application/World01.cpp
@@ -0,0 +1,53 @@
+#include "World01.h"
+#include "Framework/Framework.h"
+#include "Renderer/Renderer.h"
+#include "Input/InputSystem.h"
+
+namespace nc
+{
+    bool World01::Initialize()
+    {
+        return true;
+    }
+
+    void World01::Shutdown()
+    {
+
+    }
+
+    void World01::Update(float dt)
+    {
+        m_angle += (dt * 90);
+    }
+
+    void World01::Draw(Renderer& renderer)
+    {
+
+        // pre-render
+        renderer.BeginFrame();
+
+        // render
+        glPushMatrix();
+        //glTranslatef(0.5f, 0, 0);
+        glRotatef(m_angle, 1, 0, 0);
+
+        glBegin(GL_TRIANGLES);
+
+        glColor3f(1, 0, 0);
+        glVertex2f(-0.5f, -0.5f);
+
+        glColor3f(0, 1, 0);
+        glVertex2f(0, 0.5f);
+
+        glColor3f(0, 0, 1);
+        glVertex2f(0.5f, -0.5f);
+
+        glEnd();
+
+        glPopMatrix();
+
+
+        // post-render
+        renderer.EndFrame();
+    }
+}
